8e3e6ed748352a55e54a1414a309fd535eb82508
IamTort <61164603+IamTort@users.noreply.github.com>
2022-09-23 16:57:10 +0500

    Сделала пробник

---
+++ UIKitDz/UIKitDz/ViewControllerNew.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerNew.swift
+//  UIKitDz
+//
+//  Created by angelina on 23.09.2022.
+//
+
+import UIKit
+
+class ViewControllerNew: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
