2ae66e3a386c673bbcc077402343b8aebea1783f
IamTort <61164603+IamTort@users.noreply.github.com>
2022-09-24 15:21:38 +0500

    Сделала 4ю домашку в сториборде

---
+++ UIKitDz/UIKitDz/CheckViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CheckViewController.swift
+//  UIKitDz
+//
+//  Created by angelina on 24.09.2022.
+//
+
+import UIKit
+
+class CheckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ UIKitDz/UIKitDz/RegistrationViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegistrationViewController.swift
+//  UIKitDz
+//
+//  Created by angelina on 24.09.2022.
+//
+
+import UIKit
+
+final class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func checkAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "Выставить счет?", message: nil, preferredStyle: .alert)
+        let okAlertAction = UIAlertAction(title: "ok", style: .default) { _ in
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let checkViewController = storyboard.instantiateViewController(
+                             withIdentifier: "CheckViewController")
+            self.navigationController?.pushViewController(checkViewController, animated: true)
+        }
+        let cancelAlertAction = UIAlertAction(title: "Cancel", style: .default)
+        alertController.addAction(cancelAlertAction)
+        alertController.addAction(okAlertAction)
+        self.present(alertController, animated: true)
+    }
+}
