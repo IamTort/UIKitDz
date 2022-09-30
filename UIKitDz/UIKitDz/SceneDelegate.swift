10f648089a3f18638259dbadafef7405f14f81e9
IamTort <61164603+IamTort@users.noreply.github.com>
2022-09-30 05:02:18 +0500

    Внесла исправления 3-3.4

--- UIKitDz/UIKitDz/SceneDelegate.swift
+++ UIKitDz/UIKitDz/SceneDelegate.swift
@@ -8,7 +8,5 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 }
--- UIKitDz/UIKitDz/ViewController.swift
+++ UIKitDz/UIKitDz/ViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
-/// ViewController
-final class ViewController: UIViewController {
+/// Стартовый экран
+final class StartViewController: UIViewController {
+// MARK: - Private Properties
     private var rightNumber = 0
+    
     private lazy var nameLabel: UILabel = {
         let label = UILabel()
         label.text = "Привет, "
@@ -19,13 +21,15 @@ final class ViewController: UIViewController {
         label.clipsToBounds = true
         return label
     }()
-    private lazy var notification: UILabel = {
+    
+    private lazy var notificationLabel: UILabel = {
         let label = UILabel()
         label.text = " "
         label.textAlignment = .left
         label.frame = CGRect(x: 180, y: 250, width: 100, height: 25)
         return label
     }()
+    
     private lazy var additionButton: UIButton = {
         let button = UIButton(type: .custom)
         button.backgroundColor = .gray
@@ -33,9 +37,10 @@ final class ViewController: UIViewController {
         button.setTitle("Сложить числа", for: .normal)
         button.titleLabel?.textAlignment = .center
         button.frame = CGRect(x: 20, y: 160, width: 140, height: 60)
-        button.addTarget(self, action: #selector(addition(paramTarget:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(additionAction(paramTarget:)), for: .touchUpInside)
         return button
     }()
+    
     private lazy var resultLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
@@ -43,6 +48,7 @@ final class ViewController: UIViewController {
         label.text = "Результат: "
         return label
     }()
+    
     private lazy var findNumberButton: UIButton = {
         let button = UIButton(type: .custom)
         button.backgroundColor = .gray
@@ -51,9 +57,10 @@ final class ViewController: UIViewController {
         button.titleLabel?.numberOfLines = 0
         button.titleLabel?.textAlignment = .center
         button.frame = CGRect(x: 20, y: 240, width: 140, height: 80)
-        button.addTarget(self, action: #selector(findNumber(paramTarget:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(findNumberAction(paramTarget:)), for: .touchUpInside)
         return button
     }()
+    
     private lazy var findNumberTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Введите число от 0 до 100"
@@ -61,23 +68,28 @@ final class ViewController: UIViewController {
         textField.isHidden = true
         return textField
     }()
+    
     private lazy var tryButton: UIButton = {
         let button =  UIButton(configuration: .filled())
         button.setTitle("Применить", for: .normal)
         button.frame = CGRect(x: 20, y: 400, width: 140, height: 40)
         button.isHidden = true
-        button.addTarget(self, action: #selector(tryToFindNumber(paramTarget:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(tryToFindNumberAction(paramTarget:)), for: .touchUpInside)
         return button
     }()
+// MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
+    
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         alertGreetings()
     }
+// MARK: - Methods
     func setupUI() {
-        view.addSubview(notification)
+        view.addSubview(notificationLabel)
         view.addSubview(nameLabel)
         view.addSubview(additionButton)
         view.addSubview(resultLabel)
@@ -85,6 +97,7 @@ final class ViewController: UIViewController {
         view.addSubview(findNumberTextField)
         view.addSubview(tryButton)
     }
+    
     func alertGreetings() {
         let greetingAlertController = UIAlertController(title: "Привет",
                                                         message: "Введи своё имя",
@@ -94,40 +107,44 @@ final class ViewController: UIViewController {
         }
         greetingAlertController.addTextField()
         greetingAlertController.addAction(okAlertAction)
-        self.present(greetingAlertController, animated: true)
+        present(greetingAlertController, animated: true)
     }
-    @objc func addition(paramTarget: UIButton) {
+// MARK: - Actions
+    @objc func additionAction(paramTarget: UIButton) {
         let additionAlertController = UIAlertController(title: "Сложение",
                                                         message: "Введи 2 значения",
                                                         preferredStyle: .alert)
         additionAlertController.addTextField()
         additionAlertController.addTextField()
         let resultAlertAction = UIAlertAction(title: "Результат", style: .default) { _ in
-            guard let firstTerm = Int(additionAlertController.textFields?[0].text ?? "") else { return }
-            guard let secondTerm = Int(additionAlertController.textFields?[1].text ?? "") else { return }
+            guard
+                let firstTerm = Int(additionAlertController.textFields?[0].text ?? ""),
+                let secondTerm = Int(additionAlertController.textFields?[1].text ?? "")
+            else { return }
             self.resultLabel.text? = "Результат: " + String(firstTerm + secondTerm)
         }
         additionAlertController.addAction(resultAlertAction)
-        self.present(additionAlertController, animated: true)
+        present(additionAlertController, animated: true)
     }
-    @objc func findNumber(paramTarget: UIButton) {
-        self.tryButton.isHidden = false
-        self.findNumberTextField.isHidden = false
-        self.findNumberTextField.text = ""
-        self.rightNumber = Int.random(in: 0...100)
-        print(rightNumber)
+    
+    @objc func findNumberAction(paramTarget: UIButton) {
+        tryButton.isHidden = false
+        findNumberTextField.isHidden = false
+        findNumberTextField.text = ""
+        rightNumber = Int.random(in: 0...100)
     }
-    @objc func tryToFindNumber(paramTarget: UIButton) {
+    
+    @objc func tryToFindNumberAction(paramTarget: UIButton) {
         guard let enteredNumber = Int(findNumberTextField.text ?? "") else { return }
         switch enteredNumber {
         case rightNumber:
-            notification.text = "Угадал!"
+            notificationLabel.text = "Угадал!"
             findNumberTextField.isHidden = false
             tryButton.isHidden = false
         case ...rightNumber:
-            notification.text = "больше"
+            notificationLabel.text = "больше"
         case rightNumber...:
-            notification.text = "меньше"
+            notificationLabel.text = "меньше"
         default: break
         }
     }
