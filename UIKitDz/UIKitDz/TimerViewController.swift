83823622dea43b09191b87d5015b0b14ebc94b69
IamTort <61164603+IamTort@users.noreply.github.com>
2022-10-02 02:57:45 +0500

    Добавила марки и почистила код

--- UIKitDz/UIKitDz/TimerViewController.swift
+++ UIKitDz/UIKitDz/TimerViewController.swift
@@ -6,55 +6,41 @@
 //
 
 import UIKit
+/// Контроллер таймера
+class TimerViewController: UIViewController {
 
-class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-   
+    // MARK: - IBOutlets
     @IBOutlet weak var timePickerView: UIPickerView!
-    var hours = 1
-    var minutes = 1
-    var seconds = 1
-    var hoursPickerData = [[Int]]()
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-//        startButton.layer.cornerRadius = 35
-        
-
         timePickerView.delegate = self
         timePickerView.dataSource = self
-        
     }
-    
+}
+
+// MARK: -  UIPickerViewDelegate + extension
+extension TimerViewController: UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 3
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         switch component {
         case 0:
             return 24
-        case 1,2:
+        case 1, 2:
             return 60
         default:
             return 0
         }
     }
-    
+}
+
+// MARK: -  UIPickerViewDataSource + extension
+extension TimerViewController: UIPickerViewDataSource {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return "\(row)"
     }
-    
-    
-//    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        switch component {
-//        case 0:
-//            hours = row
-//        case 1:
-//            minutes = row
-//        case 2:
-//            seconds = row
-//        default:
-//            break
-//        }
-//    }
 }
